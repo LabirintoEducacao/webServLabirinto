174e19c34fa0ba03af2f3c2940911ecc84089c41
Luiz Carlos <frickajr@gmail.com>
2020-04-03 15:46:29 -0300

    atualização de migração do banco

--- database/migrations/2019_07_04_163516_create_role_user_table.php
+++ database/migrations/2019_07_04_163516_create_role_user_table.php
@@ -18,7 +18,6 @@ class CreateRoleUserTable extends Migration
             $table->integer('role_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->useCurrent();
         });
     }
 
